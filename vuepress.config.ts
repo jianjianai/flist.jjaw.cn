94d761aa9fb4e15050409ee9f042e729a8b39d33
jjaw <jjaw@jjaw.cn>
2024-08-08 10:05:27 +0800

    统计

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -14,7 +14,11 @@ export default defineUserConfig({
   public: `./public`,
   title: '神奇小破盘',
   description: '神奇小破盘 -- 分享各种有用的文件。',
-  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }],
+    //统计
+    ['script',{},`!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"3IG7honw86Ec9yDG",ck:"3IG7honw86Ec9yDG"});`]
+  ],
   shouldPrefetch: false,
   plugins:[
     sitemapPlugin({
